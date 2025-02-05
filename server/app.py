dd44fd05b6ea30fcd7ddb7508101bf4bbd0d06ef
GammaBeer <lakshaylegend2004@gmail.com>
2025-02-05 20:10:23 +0530

    cors issue

--- server/app.py
+++ server/app.py
@@ -8,42 +8,37 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-
-CORS(app)
-
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
 
 model = load_model('./recognition.keras')
 
 def preprocess_image(image):
-    """ Preprocess the image to fit the model input requirements. """
     image = image.convert('L')  
     image = image.resize((28, 28))  
     image = np.array(image) / 255.0  
     image = np.expand_dims(image, axis=(0, -1))  
     return image
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return "Hello, World!"
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     image_data = data.get('image')
 
-    
     if image_data:
         image_data = image_data.split(",")[1]  
         image = Image.open(BytesIO(base64.b64decode(image_data)))
 
-        
         processed_image = preprocess_image(image)
         prediction = model.predict(processed_image)
         predicted_digit = np.argmax(prediction)
 
-        return jsonify({'digit': int(predicted_digit)})
+        response = jsonify({'digit': int(predicted_digit)})
+        response.headers.add("Access-Control-Allow-Origin", "*")  # Allow CORS
+        return response
 
-    return jsonify({'error': 'No image data received'}), 400
+    response = jsonify({'error': 'No image data received'})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response, 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
